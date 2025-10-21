5e538572ed5f8fdc45c5f0212e20a11754a8a5d0
chee <yay@chee.party>
2025-10-21 17:36:23 +0100

    move description into index

--- sideboard/src/index.tsx
+++ sideboard/src/index.tsx
@@ -2,27 +2,23 @@ import { render } from "solid-js/web";
 import type { ModuleSettingsDoc } from "@patchwork/filesystem";
 import type { ToolImplementation } from "@patchwork/plugins";
 import type { TinyPatchworkAccountDoc } from "tiny-patchwork/src/lib/account-doc.ts";
-const pkg = JSON.parse(
-  await (await fetch(new URL("../package.json", import.meta.url))).text()
-);
+import style from "./style.css";
+const sheet = Object.assign(document.createElement("style"), {
+  textContent: style,
+});
+document.head.append(sheet);
 
 export const plugins = [
   {
-    type: "patchwork:tool",
-    ...pkg.patchwork.tools["chee/sideboard"],
     id: "chee/sideboard",
+    type: "patchwork:tool",
+    name: "Sideboard",
     supportedDataTypes: ["folder"],
+    icon: "FolderOpen",
     async load(): Promise<ToolImplementation<TinyPatchworkAccountDoc>> {
       const sideboard = await import("./sideboard.tsx");
-      const style = await (
-        await fetch(new URL("../style.css", import.meta.url))
-      ).text();
-
-      const sheet = new CSSStyleSheet();
-      await sheet.replace(style);
 
       return (handle, element) => {
-        document.adoptedStyleSheets = [sheet];
         return render(
           () => <sideboard.default handle={handle} repo={element.repo} />,
           element
@@ -31,20 +27,14 @@ export const plugins = [
     },
   },
   {
-    type: "patchwork:tool",
-    ...pkg.patchwork.tools["chee/module-settings"],
     id: "chee/module-settings",
+    type: "patchwork:tool",
+    name: "Module Settings",
+    icon: "Settings",
+    supportedDataTypes: ["patchwork:module-settings"],
     async load(): Promise<ToolImplementation<ModuleSettingsDoc>> {
       const modulesettings = await import("./module-settings.jsx");
-      const style = await (
-        await fetch(new URL("./style.css", import.meta.url))
-      ).text();
-
-      const sheet = new CSSStyleSheet();
-      await sheet.replace(style);
-
       return function (handle, element) {
-        document.adoptedStyleSheets = [sheet];
         return render(
           () => <modulesettings.default handle={handle} repo={element.repo} />,
           element
