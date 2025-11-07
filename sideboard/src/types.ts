cd115c0c2971c66d4507ebbbba929fd97e545b0f
chee <yay@chee.party>
2025-11-07 13:56:02 +0000

    add el to types

--- sideboard/src/types.ts
+++ sideboard/src/types.ts
@@ -1,8 +1,10 @@
 import type { AutomergeUrl, DocHandle, Repo } from "@automerge/automerge-repo";
+import type { PatchworkViewElement } from "@patchwork/element";
 
 export interface PatchworkToolProps<T> {
   handle: DocHandle<T>;
   repo: Repo;
+  element: PatchworkViewElement;
 }
 
 export type TinyPatchworkAccountDoc = {
