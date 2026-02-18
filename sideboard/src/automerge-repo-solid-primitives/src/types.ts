8f8d39a5bdae2d4447eba2020b115eeaab399205
chee <yay@chee.party>
2026-02-18 10:43:45 +0000

    remove automerge-repo-solid-primitives fork

--- sideboard/src/automerge-repo-solid-primitives/src/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Repo } from "@automerge/automerge-repo/slim"
-
-export interface UseDocHandleOptions {
-  repo?: Repo
-  // @internal
-  "~skipInitialValue"?: boolean
-}
-
-export type MaybeAccessor<T> = T | (() => T)
