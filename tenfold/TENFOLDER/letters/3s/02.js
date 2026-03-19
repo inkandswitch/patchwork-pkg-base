f756c512c2886cd808df4b93d7625b92bb982112
chee <yay@chee.party>
2026-03-19 19:32:36 +0000

    remove lib dom

--- tenfold/TENFOLDER/letters/3s/02.js
+++ tenfold/TENFOLDER/letters/3s/02.js
@@ -1,26 +1,16 @@
-/// <reference lib="dom" />
-
 // "THIS S HAS BEEN RESERVED FOR MIMI AND CHEE"
 // by Mimi & chee
 
 for (let turns = 0.35; turns < 1.935; turns += 0.01618) {
-  let dist = denorm(turns, 0.1618 * sinn(params.t / 2), 0.3);
-  let x = cosn(turns) * dist;
-  let y = sinn(turns) * dist;
-  circle(
-    x,
-    y - 0.3,
-    0.002 * sinn(turns * norm(sinn(params.t) + 0.2) * dist) * 20,
-  );
+  let dist = denorm(turns, 0.1618 * sinn(params.t / 2), 0.3)
+  let x = cosn(turns) * dist
+  let y = sinn(turns) * dist
+  circle(x, y - 0.3, 0.002 * sinn(turns * norm(sinn(params.t) + 0.2) * dist) * 20)
 }
 
 for (let turns = 0.35; turns < 1.935; turns += 0.01618) {
-  let dist = denorm(turns, 0.1618 * sinn(params.t / 2), 0.3);
-  let x = -cosn(turns) * dist;
-  let y = -sinn(turns) * dist;
-  circle(
-    x,
-    y + 0.2,
-    0.002 * sinn(turns * norm(sinn(params.t) + 0.2) * dist) * 20,
-  );
+  let dist = denorm(turns, 0.1618 * sinn(params.t / 2), 0.3)
+  let x = -cosn(turns) * dist
+  let y = -sinn(turns) * dist
+  circle(x, y + 0.2, 0.002 * sinn(turns * norm(sinn(params.t) + 0.2) * dist) * 20)
 }
